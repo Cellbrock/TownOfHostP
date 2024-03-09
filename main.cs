f98ab31cf420a2693dfc7c48f40c793d93b4ae46
Keizo-Nino <k-ninomiya@sts-inc.co.jp>
2024-03-09 23:54:09 +0900

    feat:main　papa用に変更

--- main.cs
+++ main.cs
@@ -25,17 +25,17 @@ namespace TownOfHost
     {
         // == プログラム設定 / Program Config ==
         // modの名前 / Mod Name (Default: Town Of Host)
-        public static readonly string ModName = "Town Of Host";
+        public static readonly string ModName = "Town Of Host with Papa";
         // modの色 / Mod Color (Default: #00bfff)
         public static readonly string ModColor = "#00bfff";
         // 公開ルームを許可する / Allow Public Room (Default: true)
-        public static readonly bool AllowPublicRoom = true;
+        public static readonly bool AllowPublicRoom = false;
         // フォークID / ForkId (Default: OriginalTOH)
-        public static readonly string ForkId = "OriginalTOH";
+        public static readonly string ForkId = "TOH-P";
         // Discordボタンを表示するか / Show Discord Button (Default: true)
         public static readonly bool ShowDiscordButton = true;
         // Discordサーバーの招待リンク / Discord Server Invite URL (Default: https://discord.gg/W5ug6hXB9V)
-        public static readonly string DiscordInviteUrl = "https://discord.gg/W5ug6hXB9V";
+        public static readonly string DiscordInviteUrl = "https://discord.gg/kokokarahuratsutodeiinziyanaidesuka-848546403055566858";
         // ==========
         public const string OriginalForkId = "OriginalTOH"; // Don't Change The Value. / この値を変更しないでください。
         // == 認証設定 / Authentication Config ==
